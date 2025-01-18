f88aebc0d4c19b133d6b8f271d44b3b9a6a42c04
Sarah Fisher <honestant@protonmail.com>
2025-01-18 11:55:09 +0000

    feat: enhance InvitePage to display user-specific QR codes based on role and retrieve user from local storage

--- src/app/dashboard/invite/page.tsx
+++ src/app/dashboard/invite/page.tsx
@@ -1,10 +1,45 @@
+"use client";
+
 import { HeaderOne } from "@/components";
+import {
+  EnhancedBusinessContact,
+  EnhancedCharityContact,
+  Individual,
+} from "@/types";
+import { getUserFromLocalStorage } from "@/utils/localStorage";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const InvitePage = () => {
+  const [user, setUser] = useState<
+    Individual | EnhancedBusinessContact | EnhancedCharityContact | null
+  >(null);
+
+  useEffect(() => {
+    const _user = getUserFromLocalStorage();
+    if (_user) setUser(_user);
+  }, []);
+
+  const isBusiness = user && "role" in user && user.role === "business";
+  const isCharity = user && "role" in user && user.role === "charity";
+
   return (
     <div>
-      <HeaderOne>Invite</HeaderOne>
-      <p>Allow users to send invites via email or generate referral links.</p>
+      <HeaderOne>
+        Invite a {isBusiness ? "business" : isCharity ? "charity" : "friend"}
+      </HeaderOne>
+      <Image
+        src={
+          isBusiness
+            ? "/qrcode_business.png"
+            : isCharity
+            ? "/qrcode_charity"
+            : "/qrcode.png"
+        }
+        alt='QRCode'
+        height={300}
+        width={300}
+      />
     </div>
   );
 };
